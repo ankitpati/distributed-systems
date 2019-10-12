1ddaa69e7d0a9e8eb7ddd4bbccb77fbb9732c055
Ankit Pati <contact@ankitpati.in>
2019-10-12 07:09:25 +0530

    updates, fixes, shellcheck, and fewer assumptions

--- src/rpc/test.sh
+++ src/rpc/test.sh
@@ -1,23 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-test $# -ne 2 && {
-    printf 'Usage:\n\t ./test.sh <number 1> <number 2>\n' >&2
-    exit 1
-}
+myname="$(basename "$0")"
+mydir="$(dirname "$0")/"
 
-rpcgen add.x && \
+test "$#" -ne 2 && echo "Usage: $myname <number 1> <number 2>" && exit 1
+
+cd "$mydir" || exit 2
+
+sudo systemctl start rpcbind.service
+
+rpcgen 'add.x' && \
 gcc -Wall -Wextra \
     -Wpedantic -Wno-unused-result -Wno-unused-parameter -Wno-unused-variable \
-    -o add_server addnum.c add_xdr.c add_svc.c -lnsl && \
+               -Wno-cast-function-type \
+    -o 'add_server' 'addnum.c' 'add_xdr.c' 'add_svc.c' \
+    -I'/usr/include/tirpc' -ltirpc \
+    -lnsl && \
 gcc -Wall -Wextra \
     -Wpedantic -Wno-unused-result -Wno-unused-parameter -Wno-unused-variable \
-    -o add_clnt passnum.c add_xdr.c add_clnt.c -lnsl && \
-rpcinfo &> /dev/null &&
+               -Wno-cast-function-type \
+    -o 'add_clnt' 'passnum.c' 'add_xdr.c' 'add_clnt.c' \
+    -I'/usr/include/tirpc' -ltirpc \
+    -lnsl && \
+rpcinfo &> /dev/null && \
 ./add_server &
 
 sleep 1
 
-./add_clnt localhost $@
+./add_clnt localhost "$@"
 killall add_server
 
 : '
